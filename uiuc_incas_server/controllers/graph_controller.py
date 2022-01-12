598866bba97ebee8b4684d5e5fb054c0362991e0
hrukalive <bowen.parallife@gmail.com>
2022-01-12 12:10:31 -0800

    Graph controller

--- uiuc_incas_server/controllers/graph_controller.py
+++ uiuc_incas_server/controllers/graph_controller.py
@@ -315,10 +315,11 @@ def actor_message_graph_id_put(body, id_):  # noqa: E501
                 content = util.serialize(body)
                 edges = {}
                 for i in range(len(content['edges'])):
-                    if content['edges'][i]['edgeType'] == 'actor_message':
-                        edges[f'{edge["actorId"]}-{edge["messageId"]}-{edge["edgeId"]}'] = content['edges'][i]
-                    elif content['edges'][i]['edgeType'] == 'message_actor':
-                        edges[f'{edge["messageId"]}-{edge["actorId"]}-{edge["edgeId"]}'] = content['edges'][i]
+                    edge = content['edges'][i]
+                    if edge['edgeType'] == 'actor_message':
+                        edges[f'{edge["actorId"]}-{edge["messageId"]}-{edge["edgeId"]}'] = edge
+                    elif edge['edgeType'] == 'message_actor':
+                        edges[f'{edge["messageId"]}-{edge["actorId"]}-{edge["edgeId"]}'] = edge
                     else:
                         return 'Bad request', 400
                 content['edges'] = edges
@@ -355,10 +356,11 @@ def actor_message_graph_post(body):  # noqa: E501
                     content = util.serialize(body)
                     edges = {}
                     for i in range(len(content['edges'])):
-                        if content['edges'][i]['edgeType'] == 'actor_message':
-                            edges[f'{edge["actorId"]}-{edge["messageId"]}-{edge["edgeId"]}'] = content['edges'][i]
-                        elif content['edges'][i]['edgeType'] == 'message_actor':
-                            edges[f'{edge["messageId"]}-{edge["actorId"]}-{edge["edgeId"]}'] = content['edges'][i]
+                        edge = content['edges'][i]
+                        if edge['edgeType'] == 'actor_message':
+                            edges[f'{edge["actorId"]}-{edge["messageId"]}-{edge["edgeId"]}'] = edge
+                        elif edge['edgeType'] == 'message_actor':
+                            edges[f'{edge["messageId"]}-{edge["actorId"]}-{edge["edgeId"]}'] = edge
                         else:
                             return 'Bad request', 400
                     content['edges'] = edges
